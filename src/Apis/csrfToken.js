710ae32075ae81d6522c1f34b62db8927b06bb9b
Abdullah <abdullahe0007@gmail.com>
2025-12-07 01:11:40 +0500

    Corrected the url for fetching refresh token

--- src/Apis/csrfToken.js
+++ src/Apis/csrfToken.js
@@ -7,7 +7,7 @@ export const fetchCsrfToken = async () => {
   try {
     // Refresh access token first (backend sets it in cookie)
     await axios.post(
-      "http://localhost:5000/api/auth/refresh-token",
+      "http://localhost:5000/auth/refresh-token",
       {},
       { withCredentials: true }
     );
